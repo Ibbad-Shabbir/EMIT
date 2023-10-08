726af0b3780ae79237ffd5464afc06656325285e
HU <HU@HU-DOPX-LNL19.huf.local>
2023-10-08 15:27:20 +0500

    Initial commit

---
+++ quiz-app/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the primary focus of Earth Surface Mineral Dust Source Investigation?",
+    answer: "  Investigating natural sources of mineral dust ",
+    options: [
+      "  Studying extraterrestrial dust sources",
+      " Identifying sources of volcanic ash",
+      " Investigating natural sources of mineral dust ",
+      "Analyzing oceanic dust emissions"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which scientific techniques are commonly employed to identify and study mineral dust sources on Earth's surface?",
+    answer: " Remote sensing, geospatial analysis, and mineralogical analysis",
+    options: [
+      " Baking soda experiments",
+      " Reading tea leaves",
+      " Morse code deciphering",
+      " Remote sensing, geospatial analysis, and mineralogical analysis"
+    ]
+  },
+  {
+    numb: 3,
+    question: " Earth Surface Mineral Dust Source Investigation primarily aims to understand the natural processes that generate mineral dust.",
+    answer: "True",
+    options: [
+      "False",
+      "True",
+      "ㅤㅤㅤㅤ",
+      "ㅤㅤㅤㅤ"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Why is it important to study mineral dust sources in terms of global climate and atmospheric conditions?",
+    answer: "It helps in assessing the impact on climate, air quality, and human health ",
+    options: [
+      " It has no significant impact on climate or air quality.",
+      "It only affects regional weather patterns",
+      " It's mainly for geological curiosity.",
+      "It helps in assessing the impact on climate, air quality, and human health "
+    ]
+  },
+  {
+    numb: 5,
+    question: "Name one major environmental impact associated with mineral dust emissions from natural sources.",
+    answer: " Soil erosion and degradation of air quality",
+    options: [
+      " Increased crop yields",
+      "Improved air quality",
+      "e Soil erosion and degradation of air quality",
+      "Decreased desertification"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What are some of the key factors that influence the transport and dispersion of mineral dust in the atmosphere?",
+    answer: "Wind patterns, topography, and atmospheric conditions",
+    options: [
+      " Ocean currents",
+      "Solar radiation",
+      "Wind patterns, topography, and atmospheric conditions",
+      "Underground volcanic activity"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How does Earth Surface Mineral Dust Source Investigation contribute to our understanding of regional air quality and human health?",
+    answer: " It helps identify sources of mineral dust, which can be harmful when inhaled, and contributes to air quality degradation",
+    options: [
+      " It doesn't have any relevance to air quality.",
+      "By identifying sources of mineral dust that are completely harmless",
+      " It helps identify sources of mineral dust, which can be harmful when inhaled, and contributes to air quality degradation",
+      "It only affects weather forecasting."
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which geological features or landforms are often investigated as potential mineral dust sources?",
+    answer: "Dry riverbeds, deserts, arid regions, and exposed soil surfaces",
+    options: [
+      "Rainforests",
+      "Icebergs",
+      "Mountain ranges",
+      "Dry riverbeds, deserts, arid regions, and exposed soil surfaces"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Can anthropogenic activities contribute to mineral dust emissions? (Yes/No)",
+    answer: "Yes",
+    options: [
+      " No, only natural processes can generate mineral dust.",
+      "Yes",
+      "No, mineral dust is solely a result of volcanic eruptions.",
+      "Only during lunar eclipses."
+    ]
+  },
+  {
+    numb: 10,
+    question: "What role does geospatial analysis play in EMIT?",
+    answer: "It helps in the spatial analysis of geological features and potential mineral dust sources",
+    options: [
+      "It involves the study of ancient fossils.",
+      "It helps in the spatial analysis of geological features and potential mineral dust sources",
+      "It focuses on the study of oceanography.",
+      "It involves the analysis of atmospheric gases."
+    ]
+  },
+];
